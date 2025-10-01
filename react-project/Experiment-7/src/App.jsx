79d838ef337892d35b6428e23a60a372b48a3dcd
Saket <23BAI70223@cuchd.in>
2025-10-01 22:59:22 +0530

    updates react projects

--- react-project/Experiment-7/src/App.jsx
+++ react-project/Experiment-7/src/App.jsx
@@ -1,49 +1,49 @@
-import React from "react";
-import ProductCard from "./Components/ProductCard";
+  import React from "react";
+  import ProductCard from "./Components/ProductCard";
 
-function App() {
-  const products = [
-    { name: "Wireless Mouse", price: 25.99, status: "In Stock" },
-    { name: "Keyboard", price: 45.5, status: "Out of Stock" },
-    { name: "Monitor", price: 199.99, status: "In Stock" },
-  ];
+  function App() {
+    const products = [
+      { name: "Wireless Mouse", price: 25.99, status: "In Stock" },
+      { name: "Keyboard", price: 45.5, status: "Out of Stock" },
+      { name: "Monitor", price: 199.99, status: "In Stock" },
+    ];
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",   // horizontal center
-        alignItems: "center",       // vertical center
-        height: "100vh",            // full viewport height
-      }}
-    >
+    return (
       <div
         style={{
-          border: "2px solid black",
-          padding: "20px",
-          textAlign: "center",
+          display: "flex",
+          justifyContent: "center",   // horizontal center
+          alignItems: "center",       // vertical center
+          height: "100vh",            // full viewport height
         }}
       >
-        <h2 style={{ marginBottom: "20px" }}>Products List</h2>
         <div
           style={{
-            display: "flex",
-            justifyContent: "center", // center cards
-            gap: "20px",               // spacing between cards
+            border: "2px solid black",
+            padding: "20px",
+            textAlign: "center",
           }}
         >
-          {products.map((product, index) => (
-            <ProductCard
-              key={index}
-              name={product.name}
-              price={product.price}
-              status={product.status}
-            />
-          ))}
+          <h2 style={{ marginBottom: "20px" }}>Products List</h2>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center", // center cards
+              gap: "20px",               // spacing between cards
+            }}
+          >
+            {products.map((product, index) => (
+              <ProductCard
+                key={index}
+                name={product.name}
+                price={product.price}
+                status={product.status}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default App;
+  export default App;
